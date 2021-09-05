46545e0a8baffd19bcd0186d2b2d3fe909b26e52
PratikshaNikam <53165905+PratikshaNikam@users.noreply.github.com>
2021-09-05 22:55:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+
+export  const firebaseConfig = {
+    apiKey: "AIzaSyCdq1edE41W2OuEH9QzrdCELv6cJEniq3k",
+    authDomain: "story-telling-25dbd.firebaseapp.com",
+    projectId: "story-telling-25dbd",
+    storageBucket: "story-telling-25dbd.appspot.com",
+    messagingSenderId: "724604009261",
+    appId: "1:724604009261:web:198dc3ec04c236ba0e71a4"
+  };
+
